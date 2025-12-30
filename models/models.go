9af9a6a9f1517e715f98bdfc8dec18a4c60d386c
Chinonso Alexander <chinonsoalexander2@gmail.com>
2025-12-30 18:48:40 +0100

    first commit

--- models/models.go
+++ models/models.go
@@ -183,7 +183,7 @@ type Region struct {
 	Flag         bool    `gorm:"default:true"`
 	WikiDataID   *string `gorm:"column:wikiDataId"`
 
-	Subregions []Subregion `gorm:"foreignKey:RegionID"`
+	Subregions []Subregion `gorm:"foreignKey:RegionID,omitempty"`
 }
 
 type Subregion struct {
@@ -196,8 +196,8 @@ type Subregion struct {
 	Flag         bool    `gorm:"default:true"`
 	WikiDataID   *string `gorm:"column:wikiDataId"`
 
-	Region    Region    `gorm:"foreignKey:RegionID"`
-	Countries []Country `gorm:"foreignKey:SubregionID"`
+	Region    Region    `gorm:"foreignKey:RegionID,omitempty"`
+	Countries []Country `gorm:"foreignKey:SubregionI,omitemptyD"`
 }
 
 type Country struct {
@@ -236,8 +236,8 @@ type Country struct {
 
 	RegionRef    *Region    `gorm:"foreignKey:RegionID"`
 	SubregionRef *Subregion `gorm:"foreignKey:SubregionID"`
-	States       []State    `gorm:"foreignKey:CountryID"`
-	Cities       []City     `gorm:"foreignKey:CountryID"`
+	States       []State    `gorm:"foreignKey:CountryID,omitempty"`
+	Cities       []City     `gorm:"foreignKey:CountryID,omitempty"`
 }
 
 type State struct {
@@ -262,8 +262,8 @@ type State struct {
 	WikiDataID   *string `gorm:"column:wikiDataId"`
 	Population   *string
 
-	Country Country `gorm:"foreignKey:CountryID"`
-	Cities  []City  `gorm:"foreignKey:StateID"`
+	Country Country `gorm:"foreignKey:CountryID,omitempty"`
+	Cities  []City  `gorm:"foreignKey:StateID,omitempty"`
 }
 
 type City struct {
@@ -287,8 +287,8 @@ type City struct {
 	Flag         bool    `gorm:"default:true"`
 	WikiDataID   *string `gorm:"column:wikiDataId"`
 
-	State   State   `gorm:"foreignKey:StateID"`
-	Country Country `gorm:"foreignKey:CountryID"`
+	State   State   `gorm:"foreignKey:StateID,omitempty"`
+	Country Country `gorm:"foreignKey:CountryID,omitempty"`
 }
 
 type ChatMessages struct {
