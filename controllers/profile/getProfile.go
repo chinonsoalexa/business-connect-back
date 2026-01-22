8418b888ad588e80f222756ea77f049c6ded477e
Chinonso Alexander <chinonsoalexander2@gmail.com>
2026-01-22 20:23:32 +0100

    first commit

--- controllers/profile/getProfile.go
+++ controllers/profile/getProfile.go
@@ -42,9 +42,6 @@ func GetMyProfile(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
-	// 4️⃣ Increment profile visits
-	go dbFunc.DBHelper.IncrementProfileVisits(profile.User.ID)
-
 	// 4️⃣ Return JSON
 	return ctx.JSON(fiber.Map{
 		"user":     profile.User,
